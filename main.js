bc1ea40262cbe63d255f88935762dbc87c9b282e
jharte5 <jonathan.harte@codeimmersives.com>
2019-11-21 14:14:39 -0500

    3 so far

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 const biggify = function(num) {
-  
+  return num += 9000
 }
 
 const nasafy = function(num) {
-  
+  const results = []
+  let now = num
+  while (1 <= now) {
+    results.push(now);
+    now--;
+  } 
+  results.push('Blast off!')
+  return results
 }
 
 const reversify = function(str) {
-  
+  return str.split('').reverse().join('')
 }
 
 const titleify = function(str) {
-  
+  return str.split()
 }
 
 const crazify = function(str) {
